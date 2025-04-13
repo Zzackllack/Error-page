0128083816aa62bd2a04a5d923f6e6d9a81b76f0
Zacklack <contact@zacklack.de>
2025-04-13 21:39:13 +0200

    Add animated CPU architecture visualization and resource management explanation

--- src/components/ui/cpu-architecture.tsx
+++ src/components/ui/cpu-architecture.tsx
@@ -83,6 +83,184 @@ const CpuArchitecture = ({
         )}
       </g>
 
+      {/* Flow particles - Path 1 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p1-1" r="1.2" fill="url(#cpu-blue-grad)">
+          <animateMotion
+            path="M 10 20 h 79.5 q 5 0 5 5 v 30"
+            dur="3s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+        <circle className="flow-particle flow-p1-2" r="1.2" fill="url(#cpu-blue-grad)">
+          <animateMotion
+            path="M 10 20 h 79.5 q 5 0 5 5 v 30"
+            dur="3s"
+            begin="1s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+
+      {/* Flow particles - Path 2 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p2-1" r="1.2" fill="url(#cpu-yellow-grad)">
+          <animateMotion
+            path="M 180 10 h -69.7 q -5 0 -5 5 v 30"
+            dur="2.8s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+        <circle className="flow-particle flow-p2-2" r="1.2" fill="url(#cpu-yellow-grad)">
+          <animateMotion
+            path="M 180 10 h -69.7 q -5 0 -5 5 v 30"
+            dur="2.8s"
+            begin="1.4s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+
+      {/* Flow particles - Path 3 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p3" r="1.2" fill="url(#cpu-pinkish-grad)">
+          <animateMotion
+            path="M 130 20 v 21.8 q 0 5 -5 5 h -10"
+            dur="2.2s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+
+      {/* Flow particles - Path 4 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p4" r="1.2" fill="url(#cpu-white-grad)">
+          <animateMotion
+            path="M 170 80 v -21.8 q 0 -5 -5 -5 h -50"
+            dur="2.5s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+        <circle className="flow-particle flow-p4-2" r="1.2" fill="url(#cpu-white-grad)">
+          <animateMotion
+            path="M 170 80 v -21.8 q 0 -5 -5 -5 h -50"
+            dur="2.5s"
+            begin="1.25s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+      
+      {/* Flow particles - Path 5 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p5" r="1.2" fill="url(#cpu-green-grad)">
+          <animateMotion
+            path="M 135 65 h 15 q 5 0 5 5 v 10 q 0 5 -5 5 h -39.8 q -5 0 -5 -5 v -20"
+            dur="3.5s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+
+      {/* Flow particles - Path 6 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p6-1" r="1.2" fill="url(#cpu-orange-grad)">
+          <animateMotion
+            path="M 94.8 95 v -36"
+            dur="1.8s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+        <circle className="flow-particle flow-p6-2" r="1.2" fill="url(#cpu-orange-grad)">
+          <animateMotion
+            path="M 94.8 95 v -36"
+            dur="1.8s"
+            begin="0.9s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+      
+      {/* Flow particles - Path 7 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p7" r="1.2" fill="url(#cpu-cyan-grad)">
+          <animateMotion
+            path="M 88 88 v -15 q 0 -5 -5 -5 h -10 q -5 0 -5 -5 v -5 q 0 -5 5 -5 h 14"
+            dur="3s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+        <circle className="flow-particle flow-p7-2" r="1.2" fill="url(#cpu-cyan-grad)">
+          <animateMotion
+            path="M 88 88 v -15 q 0 -5 -5 -5 h -10 q -5 0 -5 -5 v -5 q 0 -5 5 -5 h 14"
+            dur="3s"
+            begin="1.5s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+      
+      {/* Flow particles - Path 8 */}
+      <g className="flow-particles">
+        <circle className="flow-particle flow-p8" r="1.2" fill="url(#cpu-rose-grad)">
+          <animateMotion
+            path="M 30 30 h 25 q 5 0 5 5 v 6.5 q 0 5 5 5 h 20"
+            dur="2.4s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+        <circle className="flow-particle flow-p8-2" r="1.2" fill="url(#cpu-rose-grad)">
+          <animateMotion
+            path="M 30 30 h 25 q 5 0 5 5 v 6.5 q 0 5 5 5 h 20"
+            dur="2.4s"
+            begin="1.2s"
+            repeatCount="indefinite"
+            calcMode="linear"
+            keyPoints="0;1"
+            keyTimes="0;1"
+          />
+        </circle>
+      </g>
+
       {/* 1. Blue Light */}
       <g mask="url(#cpu-mask-1)">
         <circle
@@ -91,7 +269,17 @@ const CpuArchitecture = ({
           cy="0"
           r="8"
           fill="url(#cpu-blue-grad)"
-        />
+        >
+          <animate 
+            attributeName="r" 
+            values="3;8;3" 
+            dur="3s" 
+            repeatCount="indefinite" 
+            calcMode="spline"
+            keyTimes="0; 0.5; 1"
+            keySplines="0.5 0 0.5 1; 0.5 0 0.5 1"
+          />
+        </circle>
       </g>
       {/* 2. Yellow Light */}
       <g mask="url(#cpu-mask-2)">
@@ -101,7 +289,17 @@ const CpuArchitecture = ({
           cy="0"
           r="8"
           fill="url(#cpu-yellow-grad)"
-        />
+        >
+          <animate 
+            attributeName="r" 
+            values="4;8;4" 
+            dur="2.8s" 
+            repeatCount="indefinite" 
+            calcMode="spline"
+            keyTimes="0; 0.5; 1"
+            keySplines="0.5 0 0.5 1; 0.5 0 0.5 1"
+          />
+        </circle>
       </g>
       {/* 3. Pinkish Light */}
       <g mask="url(#cpu-mask-3)">
@@ -229,7 +427,17 @@ const CpuArchitecture = ({
           rx="2"
           fill="#181818"
           filter="url(#cpu-light-shadow)"
-        />
+        >
+          <animate 
+            attributeName="fill" 
+            values="#181818;#2a2a2a;#181818" 
+            dur="1s" 
+            repeatCount="indefinite"
+            calcMode="spline"
+            keyTimes="0; 0.5; 1"
+            keySplines="0.5 0 0.5 1; 0.5 0 0.5 1"
+          />
+        </rect>
         {/* CPU Text */}
         <text
           x="92"
@@ -298,38 +506,43 @@ const CpuArchitecture = ({
         </mask>
         {/* Gradients */}
         <radialGradient id="cpu-blue-grad" fx="1">
-          <stop offset="0%" stopColor="#00E8ED" />
-          <stop offset="50%" stopColor="#08F" />
+          <stop offset="0%" stopColor="#30C7FF" />
+          <stop offset="50%" stopColor="#0088FF" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-yellow-grad" fx="1">
-          <stop offset="0%" stopColor="#FFD800" />
-          <stop offset="50%" stopColor="#FFD800" />
+          <stop offset="0%" stopColor="#FFE949" />
+          <stop offset="50%" stopColor="#FFB800" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-pinkish-grad" fx="1">
-          <stop offset="0%" stopColor="#830CD1" />
-          <stop offset="50%" stopColor="#FF008B" />
+          <stop offset="0%" stopColor="#FF59F8" />
+          <stop offset="50%" stopColor="#A933FF" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-white-grad" fx="1">
-          <stop offset="0%" stopColor="white" />
+          <stop offset="0%" stopColor="#FFFFFF" />
+          <stop offset="50%" stopColor="#AAAAFF" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-green-grad" fx="1">
-          <stop offset="0%" stopColor="#22c55e" />
+          <stop offset="0%" stopColor="#4AFF91" />
+          <stop offset="50%" stopColor="#09C35B" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-orange-grad" fx="1">
-          <stop offset="0%" stopColor="#f97316" />
+          <stop offset="0%" stopColor="#FFAE35" />
+          <stop offset="50%" stopColor="#FF5C00" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-cyan-grad" fx="1">
-          <stop offset="0%" stopColor="#06b6d4" />
+          <stop offset="0%" stopColor="#35FFEA" />
+          <stop offset="50%" stopColor="#00B9C3" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <radialGradient id="cpu-rose-grad" fx="1">
-          <stop offset="0%" stopColor="#f43f5e" />
+          <stop offset="0%" stopColor="#FF5B7A" />
+          <stop offset="50%" stopColor="#FF2952" />
           <stop offset="100%" stopColor="transparent" />
         </radialGradient>
         <filter
